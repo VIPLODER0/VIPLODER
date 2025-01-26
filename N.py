22b0605efe94d0c4768088b576ff1e5828a46d12
VIPLODER0 <ankush21012005@gmail.com>
2025-01-26 16:33:54 +0530

    Update N.py

--- N.py
+++ N.py
@@ -219,7 +219,7 @@ def handle_bgmi(message):
         if user_id not in admin_id:
             # Check if the user has run the command before and is still within the cooldown period
             if user_id in bgmi_cooldown and (datetime.datetime.now() - bgmi_cooldown[user_id]).seconds < 3:
-                response = "You Are On Cooldown . Please Wait 5min Before Running The /bgmi Command Again."
+                response = "You Are On Cooldown . Please Wait 1min Before Running The /bgmi Command Again."
                 bot.reply_to(message, response)
                 return
             # Update the last time the user ran the command
