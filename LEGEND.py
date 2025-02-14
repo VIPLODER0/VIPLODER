e0039755824cf68dbe570381566c93b6c9c21cda
VIPLODER0 <ankush21012005@gmail.com>
2025-02-14 07:30:05 +0530

    Update LEGEND.py

--- LEGEND.py
+++ LEGEND.py
@@ -21,7 +21,7 @@ redeem_codes_collection = db['redeem_codes']
 attack_logs_collection = db['user_attack_logs']
 
 # Bot Configuration
-TELEGRAM_BOT_TOKEN = '8012442954:AAEdZrjhsA-aVJ8GjCJcyXQJIB70gRv6kM8'
+TELEGRAM_BOT_TOKEN = '7253955992:AAFbU1Aq9IAJ0aItHrCMFxtahOUKKWE9nOw'
 ADMIN_USER_ID = 1929943036 
 ADMIN_USER_ID = 1929943036 
 COOLDOWN_PERIOD = timedelta(minutes=1) 
