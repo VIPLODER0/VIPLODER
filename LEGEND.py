3e0dd560ad2e8a0b66a0f0dc9d2fcb95f56c15d7
VIPLODER0 <ankush21012005@gmail.com>
2025-04-11 13:31:27 +0530

    Update LEGEND.py

--- LEGEND.py
+++ LEGEND.py
@@ -23,7 +23,7 @@ redeem_codes_collection = db['redeem_codes']
 attack_logs_collection = db['user_attack_logs']
 
 # Bot Configuration
-TELEGRAM_BOT_TOKEN = '8012442954:AAGWgX-LRHTTaoyc88JZ8IRpp2Fuo7nns-Y'
+TELEGRAM_BOT_TOKEN = '7516323992:AAFw9Wtiic6t90yUN7tmSxn2VZLYHp97sLo'
 ADMIN_USER_ID = 1929943036 
 ADMIN_USER_ID = 1929943036 
 FEEDBACK_CHAT_ID = 1929943036  # Yahan admin ka Telegram ID set karein
