b25652df1bcbf9ddf9a27896eb7595737609a8a2
VIPLODER0 <ankush21012005@gmail.com>
2025-11-21 17:05:17 +0530

    Update LEGEND.py

--- LEGEND.py
+++ LEGEND.py
@@ -12,7 +12,7 @@ from pymongo import MongoClient
 from datetime import datetime, timedelta, timezone
 
 # Database Configuration
-MONGO_URI = 'mongodb+srv://Magic:Spike@cluster0.fa68l.mongodb.net/TEST?retryWrites=true&w=majority&appName=Cluster0'
+MONGO_URI = 'mongodb+srv://nedop17612:ZnXnERM6swVt16gc@cluster0.hhq4k.mongodb.net/TEST?retryWrites=true&w=majority&appName=Cluster0'
 client = MongoClient(MONGO_URI)
 db = client['TEST']
 users_collection = db['users']
@@ -1078,6 +1078,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
 
